d28fe6478afc231e5c829cef3c513e8cc62951d4
Vahid Spacer <108230053+Vahid-Spacer@users.noreply.github.com>
2024-01-21 20:00:45 +0330

    Update fix-call.sh
    1402/11/01

--- fix-call.sh
+++ fix-call.sh
@@ -9,11 +9,11 @@ read udpport
 apt update -y
 apt install git cmake -y
 
-git clone https://github.com/ambrop72/badvpn.git /root/badvpn
+git clone https://github.com/Vahid-Spacer/badvpn /root/spacer
 
-mkdir /root/badvpn/badvpn-build
+mkdir /root/spacer/badvpn-build
 
-cd  /root/badvpn/badvpn-build
+cd  /root/spacer/badvpn-build
 
 cmake .. -DBUILD_NOTHING_BY_DEFAULT=1 -DBUILD_UDPGW=1 &
 wait
